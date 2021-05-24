9770a1ec913a2d12ced7812e73fe1636620a585d
ranjanaau <>
2021-05-24 15:46:04 +0000

    Added printf statement

--- Program_1.c
+++ Program_1.c
@@ -1,1 +1,6 @@
-//Write a C Program to Display your Name
+#include<stdio.h>
+void main()
+{
+  printf("My name is Ranjana\n");
+  printf("I am from Mysore\n");
+}//Write a C Program to Display your Name
